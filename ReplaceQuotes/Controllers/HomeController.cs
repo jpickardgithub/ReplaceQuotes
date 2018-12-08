794657e3b4c81408d8b308984bd0bd4acd93e6cc
Julian Pickard <jpickard.inbox@gmail.com>
2018-12-08 15:21:46 +0000

    added

---
+++ ReplaceQuotes/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ReplaceQuotes.Models;
+
+namespace ReplaceQuotes.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            TextModel model = new TextModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult index(TextModel model)
+        {
+            TextModel output = new TextModel();
+            if (!string.IsNullOrWhiteSpace(model.InputText))
+            {
+                output.InputText = model.InputText;
+                string inputText = model.InputText;
+                output.OutputText = Replace(inputText);
+            }
+            return View(output);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private string Replace(string input)
+        {
+            string output = input.Replace("\"", "'");
+            return output;
+        }
+    }
+}
---
+++ ReplaceQuotes/Models/TextModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReplaceQuotes.Models
+{
+    public class TextModel
+    {
+        public string InputText { get; set; }
+        public string OutputText { get; set; }
+    }
+}
